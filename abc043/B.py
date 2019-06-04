34bb0dc075a8e2bdc84ea0ca9d6652240c83bfb5
n07 <wac6et@gmail.com>
2019-06-04 20:48:32 +0900

    abc
    ABC043 B.py
    ABC061 A.py

---
+++ abc043/B.py
@@ -0,0 +1,11 @@
+s = input()
+s_l = []
+
+for i in range(0, len(s)):
+    if s[i] == 'B' and len(s_l) > 0:
+        s_l.pop()
+    elif s[i] != 'B':
+        s_l.append(s[i])
+        
+print(''.join(s_l))
+
---
+++ abc061/A.py
@@ -0,0 +1,7 @@
+A, B, C = map(int, input().split(' '))
+
+if A <= C and C <= B:
+    print('Yes')
+else:
+    print('No')
+
