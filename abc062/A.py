e4bb8531086dc350f81b3a3b23948df8c6f69bb2
n07 <wac6et@gmail.com>
2019-06-08 16:14:47 +0900

    ABC
    062 064

---
+++ abc062/A.py
@@ -0,0 +1,22 @@
+def resolve():
+    x, y = map(int, input().split(' '))
+
+    group_a = [1, 3, 5, 7, 8, 10, 12]
+    group_b = [4, 6, 9, 11]
+    group_c = [2]
+
+    if (x in group_a) and (y in group_a):
+        print('Yes')
+        return
+
+    elif (x in group_b) and (y in group_b):
+        print('Yes')
+        return
+
+    elif (x in group_c) and (y in group_c):
+        print('Yes')
+        return
+
+    print('No')
+
+resolve()
