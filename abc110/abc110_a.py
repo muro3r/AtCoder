108497c298b88bc374e5729fb00c38bc56757f8d
n07 <wac6et@gmail.com>
2019-10-05 00:57:25 +0900

    abc

--- abc110/abc110_a.py
+++ abc110/abc110_a.py
@@ -1,17 +1,21 @@
 '''A - Maximize the Formula
 https://atcoder.jp/contests/abc110/tasks/abc110_a
 
->>> main([1,5,2])
+>>> main([1, 5, 2])
 53
->>> main([9,9,9])
+>>> main([9, 9, 9])
 108
->>> main([6,6,7])
+>>> main([6, 6, 7])
 82'''
 
+
 def main(i):
-    _i = sorted(i)
-    print(int(''.join(_i[:1])) + _i[2])
-    
+    _i = sorted(i, reverse=True)
+    head = str(_i[0]) + str(_i[1])
+
+    print(int(head) + _i[2])
+
+
 if __name__ == '__main__':
     i = [i for i in map(int, input().split(' '))]
     main(i)
