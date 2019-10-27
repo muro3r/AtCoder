fe8950b5ebf19df83edb40583042d6b8a9be8a5b
muro3 <wac6et@gmail.com>
2019-10-27 14:48:10 +0900

    ABC

---
+++ apc001/apc001_a.py
@@ -0,0 +1,22 @@
+'''A - Two Integers
+https://atcoder.jp/contests/apc001/tasks/apc001_a
+
+>>> main(8, 6)
+8
+>>> main(3, 3)
+-1
+
+'''
+
+
+def main(x, y):
+    if x % y == 0:
+        print(-1)
+    else:
+        print(x)
+
+
+if __name__ == '__main__':
+    x, y = map(int, input().split(' '))
+
+    main(x, y)
